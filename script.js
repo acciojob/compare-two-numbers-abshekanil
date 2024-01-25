044e71778aea791435ec1ba2dcc089bdd40fe298
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2024-01-25 17:05:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let num1 = prompt('Enter first number');
+let num2 = prompt('Enter second number');
+
+if(parseInt(num1) === parseInt(num2))
+{
+	alert(true);
+} else
+{
+	alert(false);
+} 
